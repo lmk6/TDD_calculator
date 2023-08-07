6b416280c74f1975cb3ed5fc374e77d0513a8b00
lmk6 <lmk6@aber.ac.uk>
2023-08-07 13:36:07 +0100

    Improved tests for CalculatorMode; implemented custom IllegalValueException

--- MVC/CalculatorModel.py
+++ MVC/CalculatorModel.py
@@ -1,6 +1,35 @@
+class IllegalValueException(Exception):
+    def __init__(self, value):
+        super().__init__(f"Cannot Perform operations on the received value! -- <{value}>")
+
+
 class CalculatorModel:
+
     def add(self, *args):
+        self.check_for_illegal_value(args)
         return sum(args)
 
     def subtract(self, *args):
+        self.check_for_illegal_value(args)
         return args[0] - sum(args[1:])
+
+    def multiply(self, *args):
+        self.check_for_illegal_value(args)
+        result = args[0]
+        for arg in args[1:]:
+            result *= arg
+
+        return result
+
+    def divide(self, *args):
+        self.check_for_illegal_value(args)
+        result = args[0]
+        for arg in args[1:]:
+            result /= arg
+
+        return result
+
+    def check_for_illegal_value(self, args):
+        for arg in args:
+            if not isinstance(arg, (float, int)):
+                raise IllegalValueException(arg)
--- MVC_tests/ModelTests.py
+++ MVC_tests/ModelTests.py
@@ -1,5 +1,5 @@
 import unittest
-from MVC.CalculatorModel import CalculatorModel
+from MVC.CalculatorModel import *
 
 
 class ModelTests(unittest.TestCase):
@@ -14,8 +14,36 @@ class ModelTests(unittest.TestCase):
         self.assertEqual(self.model.add(0, 0), 0)
         self.assertEqual(self.model.add(0, -1), -1)
 
+    def test_illegal_value_exception(self):
+        with self.assertRaises(IllegalValueException):
+            self.model.add('a', 0)
+
+        with self.assertRaises(IllegalValueException):
+            self.model.subtract('b', 1)
+
+        with self.assertRaises(IllegalValueException):
+            self.model.multiply('c', 2)
+
+        with self.assertRaises(IllegalValueException):
+            self.model.divide('d', 3)
+
     def test_subtract(self):
         self.assertEqual(self.model.subtract(1, 3), -2)
+        self.assertEqual(self.model.subtract(1, 1, 1), -1)
+        self.assertEqual(self.model.subtract(1, -2), 3)
+
+    def test_multiply(self):
+        self.assertEqual(self.model.multiply(1, 1), 1)
+        self.assertEqual(self.model.multiply(3, 3), 9)
+        self.assertEqual(self.model.multiply(5, 0), 0)
+        self.assertEqual(self.model.multiply(-1, -5), 5)
+        self.assertEqual(self.model.multiply(-1, -5, -3), -15)
+
+    def test_divide(self):
+        self.assertEqual(self.model.divide(1, 1), 1)
+        self.assertEqual(self.model.divide(2, -2), -1)
+        self.assertEqual(self.model.divide(-3, -3), 1)
+        self.assertEqual(self.model.divide(-6, 3, -2), 1)
 
 
 if __name__ == "__name__":
